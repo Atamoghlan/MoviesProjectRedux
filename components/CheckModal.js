7fe368268e259898e6f5d03d589cff6c9f11ecfd
atamoghlan <atamoghlan.a.aliyev@gmail.com>
2021-01-05 14:31:43 +0400

    4

--- components/CheckModal.js
+++ components/CheckModal.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { View ,Image, Text, TouchableOpacity, ScrollView, Modal, StyleSheet} from 'react-native';
-import { connect } from "react-redux";
 
 export const CheckModal = (props) => {
     const {icon, name, description, onPress, modalWindow} = props;
--- components/SearchMovie.js
+++ components/SearchMovie.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {View, Text, TextInput, TouchableOpacity,StyleSheet } from 'react-native';
-import { connect } from "react-redux";
 
 export const SearchMovie = (props) => {
     const {handledChangedText , findMovie} = props;
